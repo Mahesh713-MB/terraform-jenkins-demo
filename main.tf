279051e8e220bd7c5902998b1e090b6c8c245a11
Mahesh713-MB <enterpriseslaxmi379@gmail.com>
2026-09-10 11:45:56 +0530

    Update Terraform configuration for Jenkins demo

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
-resource "local_file" "my_pet" {
-  filename = var.filename
-  content  = var.content
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
 }
 
-resource "random_pet" "petname" {
-  prefix    = "MR"
-  separator = "."
-  length    = 1
+resource "local_file" "jenkins_demo" {
+  filename = "jenkins-demo.txt"
+  content  = "Terraform executed successfully from Jenkins!"
 }
